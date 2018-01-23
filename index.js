dab55147e53033dd127531b6aa9c48fe2dca6ece
Matías Lescano <mjlescano@protonmail.com>
2018-01-23 17:25:23 -0300

    Update rules

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb-base'
-  ]
+  ],
+
+  rules: {
+    'consistent-return': 0,
+    'no-shadow': ['error', { 'allow': ['done', 'err', 'cb'] }],
+  }
 }
